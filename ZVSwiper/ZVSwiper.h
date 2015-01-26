69da420cfa6c895929c968d7ab0af5cedf67827a
minniger <dave@minnigerode.org>
2015-01-25 19:50:10 -0600

    Initial Commit

---
+++ ZVSwiper/ZVSwiper.h
@@ -0,0 +1,19 @@
+//
+//  ZVSwiper.h
+//  ZVSwiper
+//
+//  Created by M. David Minnigerode on 1/25/15.
+//  Copyright (c) 2015 M. David Minnigerode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZVSwiper.
+FOUNDATION_EXPORT double ZVSwiperVersionNumber;
+
+//! Project version string for ZVSwiper.
+FOUNDATION_EXPORT const unsigned char ZVSwiperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZVSwiper/PublicHeader.h>
+
+
